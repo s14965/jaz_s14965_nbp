1fba41b0a7cb4ad71b56007d67db07752f2988cf
s14965 <s14965@pjwstk.edu.pl>
2021-07-04 16:14:51 +0200

    FINAL commit

--- src/main/java/pl/pjatk/szymaj/NBP/NBPRepository.java
+++ src/main/java/pl/pjatk/szymaj/NBP/NBPRepository.java
@@ -2,5 +2,5 @@ package pl.pjatk.szymaj.NBP;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NBPRepository  extends JpaRepository<Gold, Long> {
+public interface NBPRepository  extends JpaRepository<DailyRate, Long> {
 }
